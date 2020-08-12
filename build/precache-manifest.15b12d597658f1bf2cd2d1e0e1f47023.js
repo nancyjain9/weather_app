c34fb66387a97bdc6b9382215b184ff2b3ffa045
nancy jain <nancy.jain@avizva.com>
2020-08-12 15:15:05 +0530

    build

---
+++ build/precache-manifest.15b12d597658f1bf2cd2d1e0e1f47023.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7087c2ec6df29b9cddeb7da7aa3642fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b12f8e381170f4ad0955",
+    "url": "/static/css/main.d8461c1c.chunk.css"
+  },
+  {
+    "revision": "fb3a6362e4374842b30e",
+    "url": "/static/js/2.b6b9b082.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.b6b9b082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12f8e381170f4ad0955",
+    "url": "/static/js/main.3410f8f2.chunk.js"
+  },
+  {
+    "revision": "c1c2885adb41754fb5c2",
+    "url": "/static/js/runtime-main.ef333745.js"
+  },
+  {
+    "revision": "a8528a868d461f09ee29a47e4c5323c8",
+    "url": "/static/media/Clear.a8528a86.png"
+  },
+  {
+    "revision": "c6d1cb3616b92442a99eb3acd4063a89",
+    "url": "/static/media/LightCloud.c6d1cb36.png"
+  },
+  {
+    "revision": "b62e950be97488071f86695792d1a919",
+    "url": "/static/media/Shower.b62e950b.png"
+  },
+  {
+    "revision": "e1202bb6505e7b4c7a9fac14eafe4162",
+    "url": "/static/media/Sleet.e1202bb6.png"
+  }
+]);
